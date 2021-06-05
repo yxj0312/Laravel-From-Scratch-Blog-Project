1477c3799ba88cbb707c2072b6a5315b25f687a9
yxj0312 <yanxiajun@hotmail.com>
2021-06-05 22:28:17 +0200

    add store function

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -10,4 +11,20 @@ class RegisterController extends Controller
     {
         return view('register.create');
     }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:7|max:255',
+        ]);
+
+        User::create($attributes);
+
+        return redirect('/');
+    }
 }
+
+
