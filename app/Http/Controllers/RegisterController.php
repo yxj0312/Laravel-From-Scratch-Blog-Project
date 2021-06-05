8083a5dc1fa6993e816b376340d07089f06e4377
yxj0312 <yanxiajun@hotmail.com>
2021-06-05 21:24:43 +0200

    add create function

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function create()
+    {
+        return 'hello world';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
