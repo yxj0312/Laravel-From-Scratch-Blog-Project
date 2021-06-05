bd4501b365267e757e70e54bb55c3598a70953f5
yxj0312 <yanxiajun@hotmail.com>
2021-06-05 22:27:20 +0200

    add router

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('register', [RegisterController::class, 'create']);
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
