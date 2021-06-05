e853e6478cab4916b5f68d1dc0b21baa34b41a48
yxj0312 <yanxiajun@hotmail.com>
2021-06-05 20:56:50 +0200

    make a register controller

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
