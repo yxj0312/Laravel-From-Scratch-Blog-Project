43af285848a31a76203c6ce241287ca1eb6435f5
yxj0312 <yanxiajun@hotmail.com>
2021-06-07 21:16:50 +0200

    add session to redirect

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,7 +23,7 @@ class RegisterController extends Controller
 
         User::create($attributes);
 
-        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created.');;
     }
 }
 
