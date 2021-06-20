af4fb0e6d29ce6cd1d8af0d573f2b592a6b75306
yxj0312 <yanxiajun@hotmail.com>
2021-06-20 22:33:34 +0200

    add relationship and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -19,8 +20,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'John Doe'
         ]);
 
-        Post::factory(5)->create([
-            'user_id' => $user->id
+        Comment::factory(5)->create([
+            'user_id' => $user->id,
+            'post_id' => Post::factory()->create()->id
         ]);
     }
 }
