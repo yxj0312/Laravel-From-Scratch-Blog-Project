5cbd4250041c34eb858687ceec8c3704963a255f
yxj0312 <yanxiajun@hotmail.com>
2021-07-02 22:44:48 +0200

    add a form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,6 +50,39 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <form method="POST" action="#" class="border border-gray-200 p-6 rounded-xl">
+                        @csrf
+
+                        <header class="flex items-center">
+                            <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}"
+                                alt=""
+                                width="40"
+                                height="40"
+                                class="rounded-full">
+
+                            <h2 class="ml-4">Want to participate?</h2>
+                        </header>
+
+                        <div class="mt-6">
+                            <textarea
+                                name="body"
+                                class="w-full text-sm focus:outline-none focus:ring"
+                                rows="5"
+                                placeholder="Quick, thing of something to say!"
+                                required></textarea>
+
+                            @error('body')
+                                <span class="text-xs text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                            <x-submit-button>Post</x-submit-button>
+                        </div>
+                    </form>
+
+
+
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
                     @endforeach
