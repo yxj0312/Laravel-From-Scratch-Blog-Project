afa51ea49f0c5584d48a5c662792e9a8e466cdfd
yxj0312 <yanxiajun@hotmail.com>
2021-07-04 23:10:11 +0200

    add import

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
