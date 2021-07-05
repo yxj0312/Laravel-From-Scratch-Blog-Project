4f5f997804edeba0f1d52725b62c3119550c77de
yxj0312 <yanxiajun@hotmail.com>
2021-07-05 21:29:10 +0200

    add store method to postcomment controller

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -6,8 +6,17 @@ use Illuminate\Http\Request;
 
 class PostCommentsController extends Controller
 {
-    public function store()
+    public function store(Post $post)
     {
-        
+        request()->validate([
+            'body' => 'required'
+        ]);
+
+        $post->comments()->create([
+            'user_id' => request()->user()->id,
+            'body' => request('body')
+        ]);
+
+        return back();
     }
 }
