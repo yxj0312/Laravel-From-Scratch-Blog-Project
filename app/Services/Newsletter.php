797de43fd1752706f72c485f7868eb41acf59ebb
yxj0312 <yanxiajun@hotmail.com>
2021-07-21 22:52:55 +0200

    add refactor suggestion

--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -18,6 +18,7 @@ class Newsletter
 
     protected function client()
     {
+        // you can also inject the config in the constructor
         return (new ApiClient())->setConfig([
             'apiKey' => config('services.mailchimp.key'),
             'server' => 'us6'
