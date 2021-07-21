9aae0145d7ed23d5c61aeceb7569e417a3df4193
yxj0312 <yanxiajun@hotmail.com>
2021-07-21 23:01:43 +0200

    create a invokeable controller for single method

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,17 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
-
-Route::post('newsletter', function(Newsletter $newsletter) {
-    request()->validate(['email' => 'required|email']);
-
-    
-
-    // $response = $mailchimp->lists->getAllLists();
-
-    try {
-        $newsletter->subscribe(request('email'));
-    } catch (Exception $e) {
-        throw ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.'
-        ]);
-    }
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
-});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
