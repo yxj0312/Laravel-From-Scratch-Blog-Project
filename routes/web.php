5f1858883f5714497f78b150403208e5ea8854ca
yxj0312 <yanxiajun@hotmail.com>
2021-07-31 22:37:46 +0200

    register middleware and add it to router

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
-Route::get('admin/posts/create', [PostController::class, 'create']);
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
