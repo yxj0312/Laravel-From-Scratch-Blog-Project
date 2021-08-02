ebb1f66cf3256d350ba8a5b07c2fa01bd713beb4
yxj0312 <yanxiajun@hotmail.com>
2021-08-02 22:13:24 +0200

    add thumbnail

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -48,6 +48,25 @@
                     @enderror
                 </div>
 
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="thumbnail"
+                    >
+                        Thumbnail
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="file"
+                           name="thumbnail"
+                           id="thumbnail"
+                           required
+                    >
+
+                    @error('thumbnail')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
