2030e9300c69e91c363b9deaac9fd6950d05e449
yxj0312 <yanxiajun@hotmail.com>
2021-08-10 22:56:29 +0200

    change route action

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
-Route::post('admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
+Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
 Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
 Route::post('admin/posts', [PostController::class, 'store'])->middleware('admin');
