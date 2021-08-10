b180a042387545000ebea0466d228586a0a68230
yxj0312 <yanxiajun@hotmail.com>
2021-08-10 22:09:15 +0200

    add action and blade file

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class AdminPostController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.posts.index', [
+            'posts' => Post::paginate(50)
+        ]);
+    }
 }
