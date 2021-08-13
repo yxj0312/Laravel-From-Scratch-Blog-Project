3d637f074e06e4561a92f521a4fae8afe4efcea9
yxj0312 <yanxiajun@hotmail.com>
2021-08-13 23:21:11 +0200

    move create and store method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,35 +23,4 @@ class PostController extends Controller
             'post' => $post
         ]);
     }
-
-    public function create()
-    {
-        // if (auth()->guest()) {
-        //     abort(Response::HTTP_FORBIDDEN);
-        // }
-
-        // if (auth()->user()?->username !== 'JeffreyWay') {
-        //     abort(Response::HTTP_FORBIDDEN);
-        // }
-        return view('admin.posts.create');
-    }
-
-    public function store()
-    {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'thumbnail' => 'required|image',
-            'slug' => ['required', Rule::unique('posts', 'slug')],
-            'excerpt' => 'required',
-            'body' => 'required',
-            'category_id' => ['required', Rule::exists('categories', 'id')]
-        ]);
-
-        $attributes['user_id'] = auth()->id();
-        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
-
-        Post::create($attributes);
-
-        return redirect('/');
-    }
 }
