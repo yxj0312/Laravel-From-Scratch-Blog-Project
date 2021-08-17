f5d60b22ce5e4b8cfa16faa20926ce77750df261
yxj0312 <yanxiajun@hotmail.com>
2021-08-17 23:01:06 +0200

    add destroy router

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,4 @@ Route::post('admin/posts', [PostController::class, 'store'])->middleware('admin'
 Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
 Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
 Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
+Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
