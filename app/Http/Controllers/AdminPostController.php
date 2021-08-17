41191382f55e46683a592a324cff5f0ae4f1643d
yxj0312 <yanxiajun@hotmail.com>
2021-08-17 23:00:36 +0200

    add destory method

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -69,4 +69,11 @@ class AdminPostController extends Controller
 
         return back()->with('success', 'Post Updated!');
     }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return back()->with('success', 'Post Deleted!');
+    }
 }
